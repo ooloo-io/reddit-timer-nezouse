f6d7bc3833ace6d64a0420149b6f7c8c20a4bc48
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2020-08-21 00:35:21 +0200

    Create app skeleton (#1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyles';
+import theme from './theme';
 
-function App() {
-  return (
-    <div>
-      App Placeholder
-    </div>
-  );
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/search">
+          <div>
+            Search
+          </div>
+        </Route>
+        <Route path="/">
+          <div>
+            Homepage
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>
+);
 
 export default App;
