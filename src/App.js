5cdb67f150b12e21505ae07324850115d59eab0d
nezouse <stankiewicz1998@gmail.com>
2020-08-24 10:19:24 +0200

    Add all elements of header to the page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyles';
 import theme from './theme';
+import Header from './components/Header';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <Router>
       <GlobalStyle />
+      <Header />
       <Switch>
         <Route path="/search">
           <div>
