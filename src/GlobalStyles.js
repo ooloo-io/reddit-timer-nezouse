1a74772da6ef9e946aa97f36d9274b12c28a137a
nezouse <stankiewicz1998@gmail.com>
2020-09-03 14:29:49 +0200

    Fix global styles so rems work correctly

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,11 +4,11 @@ import { normalize } from 'styled-normalize';
 const GlobalStyle = createGlobalStyle`
     ${normalize};
 
-    body {
+    html {
         font-family: ${(props) => props.theme.font.family.default};
         font-size: ${(props) => props.theme.font.size.default};
         line-height: ${(props) => props.theme.font.lineHeight.default};
-        color: ${(props) => props.theme.color.default};
+        color: ${(props) => props.theme.color.text};
     }
 `;
 
